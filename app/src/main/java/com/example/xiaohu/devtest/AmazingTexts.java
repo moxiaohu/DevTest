3a8f1639bf04b91f0449b9a2eb5edb30f86a1387
moxiaohu <xiaohumo.efrei@gmail.com>
2015-05-04 18:37:25 +0200

    start doing text widget

--- app/src/main/java/com/example/xiaohu/devtest/AmazingTexts.java
+++ app/src/main/java/com/example/xiaohu/devtest/AmazingTexts.java
@@ -1,68 +1,314 @@
 package com.example.xiaohu.devtest;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.Gravity;
+import android.text.InputType;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 
 /**
  * Created by xiaohu on 30/04/15.
  */
-public class AmazingTexts extends Activity implements View.OnTouchListener{
+public class AmazingTexts extends Activity {
 
-    private static final String EDITTEXT_TAG = "edit_text";
-    private GestureDetector mGestureDetector;
+    @InjectView(R.id.text_size_Bar)
+    SeekBar txtSizeBar;
+
+    @InjectView(R.id.new_text_button)
+    Button newTxtBtn;
+
+    @InjectView(R.id.size_text_button)
+    Button sizeTxtBtn;
+
+    @InjectView(R.id.text_size_Bar_layout)
+    LinearLayout txtSizeBarLayout;
+
+    @InjectView(R.id.text_toolbar_layout)
+    LinearLayout txtTBarLayout;
 
 
+    private ArrayList<EditText> editTexts = new ArrayList<EditText>();
+    private  int screenWidth;
+    private  int screenHeight;
+    private EditText currentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+         screenWidth = dm.widthPixels;
+         screenHeight = dm.heightPixels;
+
         setContentView(R.layout.amazing_text_layout);
         ButterKnife.inject(this);
+
+        setTxtSizeBarListener();
+        setImageBackgroundWithTxt();
+
     }
 
-    @OnClick(R.id.create_text_button)
-    public void clickCreateTxtRBtn() {
-
-        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.content_layout);
-        final EditText editText = new EditText(this);
-        editText.setHint("tape to edit");
-        editText.setBackgroundColor(Color.parseColor("#ffffff"));
-        editText.setTag(EDITTEXT_TAG);
-
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.addRule(Gravity.CENTER);
-        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-        editText.setLayoutParams(layoutParams);
-        relativeLayout.addView(editText);
-
-        editText.setOnLongClickListener(new View.OnLongClickListener() {
+    private void setTxtSizeBarListener() {
+        txtSizeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public boolean onLongClick(View v) {
-//                ClipData.Item item = new ClipData.Item(v.getTag().toString());
-                View.DragShadowBuilder myShadow = new MyDragShadowBuilder(editText);
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (editTexts.size() != 0){
+                    int txtSize = progress;
+                    currentTxt.setTextSize(txtSize);
+                }
+            }
 
-                v.startDrag(null, myShadow, null, 0);
-                return false;
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        mGestureDetector.onTouchEvent(event);
-        return true;
+    private void setImageBackgroundWithTxt() {
+        Bitmap bitmap = drawTextToBitmap(getApplicationContext(), R.drawable.earth, "earth");
+        RelativeLayout contentLayout = (RelativeLayout) findViewById(R.id.content_layout);
+        contentLayout.setBackground(new BitmapDrawable(getResources(), bitmap));
+    }
+
+    private View.OnTouchListener createOnTouchListener() {
+
+
+
+        View.OnTouchListener textListener = new View.OnTouchListener() {
+            int lastX
+                    ,
+                    lastY;
+            boolean lastActionIsMove;
+
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                int action = event.getAction();
+                switch (action) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        lastX = (int) event.getRawX();
+                        lastY = (int) event.getRawY();
+                        lastActionIsMove = false;
+                        currentTxt = (EditText)v;
+                        currentTxt.setBackgroundResource(R.drawable.blur);
+                        for (int i = 0; i < editTexts.size(); i ++){
+                            if (!currentTxt.equals(editTexts.get(i))){
+                                editTexts.get(i).setBackgroundColor(Color.TRANSPARENT);
+                            }
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        lastActionIsMove = true;
+                        int dx = (int) event.getRawX() - lastX;
+                        int dy = (int) event.getRawY() - lastY;
+                        int left = v.getLeft() + dx;
+                        int top = v.getTop() + dy;
+                        int right = v.getRight() + dx;
+                        int bottom = v.getBottom() + dy;
+                        if (left < 0) {
+                            left = 0;
+                            right = left + v.getWidth();
+                        }
+                        if (right > screenWidth) {
+                            right = screenWidth;
+                            left = right - v.getWidth();
+                        }
+                        if (top < 0) {
+                            top = 0;
+                            bottom = top + v.getHeight();
+                        }
+                        if (bottom > screenHeight) {
+                            bottom = screenHeight;
+                            top = bottom - v.getHeight();
+                        }
+                        v.layout(left, top, right, bottom);
+                        Log.i("", "position:" + left + ", " + top + ", " + right
+                                + ", " + bottom);
+                        ((RelativeLayout.LayoutParams) (v.getLayoutParams())).leftMargin = left;
+                        ((RelativeLayout.LayoutParams) (v.getLayoutParams())).topMargin = top;
+                        lastX = (int) event.getRawX();
+                        lastY = (int) event.getRawY();
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        if ((int) event.getRawX() == lastX && (int) event.getRawY() == lastY && !lastActionIsMove) {
+                            Log.i("", "can edit now:");
+                            setTxtInPutDialog((EditText)v);
+                        }
+                        break;
+                }
+                return true;
+            }
+        };
+        return textListener;
+    }
+
+    private void setTxtInPutDialog(final EditText editText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("please input your text");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                editText.setText(input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    @OnClick(R.id.new_text_button)
+    public void clickCreateTxtRBtn() {
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.content_layout);
+        EditText editText = new EditText(this);
+        editText.setHint("tape to edit");
+        editText.setBackgroundResource(R.drawable.blur);
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(screenWidth/3, screenWidth/3);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+//        layoutParams.addRule(RelativeLayout.ALIGN_TOP);
+        editText.setLayoutParams(layoutParams);
+        relativeLayout.addView(editText);
+
+        editText.setOnTouchListener(createOnTouchListener());
+        editTexts.add(editText);
+        currentTxt = editTexts.get(editTexts.size() - 1);
+        for(int i = 0; i < editTexts.size() - 1; i ++){
+            editTexts.get(i).setBackgroundColor(Color.TRANSPARENT);
+        }
+    }
+
+    @OnClick(R.id.text_widget_button)
+    public void clickTxtWidgetBtn(){
+        if (txtTBarLayout.getVisibility() == View.INVISIBLE){
+            txtTBarLayout.setVisibility(View.VISIBLE);
+            txtSizeBarLayout.setVisibility(View.INVISIBLE);
+            onlyShowCurrentEdtTxt();
+        }else {
+            txtTBarLayout.setVisibility(View.INVISIBLE);
+            txtSizeBarLayout.setVisibility(View.INVISIBLE);
+            closeAllEdtTxtBackground();
+        }
+    }
+
+    private void onlyShowCurrentEdtTxt() {
+        if (editTexts.size() != 0){
+            currentTxt.setBackgroundResource(R.drawable.blur);
+            for (int i = 0; i < editTexts.size(); i ++){
+                if (!currentTxt.equals(editTexts.get(i))){
+                    editTexts.get(i).setBackgroundColor(Color.TRANSPARENT);
+                }
+            }
+        }
+    }
+
+    private void closeAllEdtTxtBackground() {
+        if (editTexts.size() != 0){
+            for (int i = 0; i < editTexts.size(); i ++){
+                editTexts.get(i).setBackgroundColor(Color.TRANSPARENT);
+            }
+        }
+    }
+
+    @OnClick(R.id.button_ok_text_widget)
+    public void clickTxtWidgetOKBtn(){
+        txtTBarLayout.setVisibility(View.INVISIBLE);
+        closeAllEdtTxtBackground();
     }
 
 
+    @OnClick(R.id.size_text_button)
+    public void clickOnTxtSizeBtn(){
+        txtSizeBarLayout.setVisibility(View.VISIBLE);
+        txtTBarLayout.setVisibility(View.INVISIBLE);
+    }
+
+    @OnClick(R.id.button_ok_text_size)
+    public void clickOnTxtSizeOKBtn(){
+        txtSizeBarLayout.setVisibility(View.INVISIBLE);
+        txtTBarLayout.setVisibility(View.VISIBLE);
+    }
+
+    public Bitmap drawTextToBitmap(Context gContext,
+                                   int gResId,
+                                   String gText) {
+        Resources resources = gContext.getResources();
+        float scale = resources.getDisplayMetrics().density;
+        Bitmap bitmap =
+                BitmapFactory.decodeResource(resources, gResId);
+
+        android.graphics.Bitmap.Config bitmapConfig =
+                bitmap.getConfig();
+        // set default bitmap config if none
+        if(bitmapConfig == null) {
+            bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
+        }
+        // resource bitmaps are imutable,
+        // so we need to convert it to mutable one
+        bitmap = bitmap.copy(bitmapConfig, true);
+
+        Canvas canvas = new Canvas(bitmap);
+        // new antialised Paint
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // text color - #3D3D3D
+        paint.setColor(Color.WHITE);
+        // text size in pixels
+        paint.setTextSize((int) (30 * scale));
+
+        paint.setTypeface(Typeface.MONOSPACE);
+
+        // draw text to the Canvas center
+        Rect bounds = new Rect();
+        paint.getTextBounds(gText, 0, gText.length(), bounds);
+        int x = (bitmap.getWidth() - bounds.width())/2;
+        int y = (bitmap.getHeight() + bounds.height())/2;
+
+        canvas.drawText(gText, x, y, paint);
+
+        return bitmap;
+    }
 }
--- app/src/main/java/com/example/xiaohu/devtest/MainActivity.java
+++ app/src/main/java/com/example/xiaohu/devtest/MainActivity.java
@@ -1,196 +1,104 @@
-package com.example.xiaohu.devtest;
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.PointF;
-import android.os.Bundle;
-import android.util.FloatMath;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-/**
- * 缩放图片界面
- * @author ZHF
- *
- */
-public class MainActivity extends Activity {
-    public static final String TAG = "ImgDisplayActivity";
-    //控件声明
-    private Button btnZoomin, btnZoomout;
-    private EditText edtTxtDisplay;
-    private RelativeLayout lLayoutDisplay;
-    private FrameLayout fLayoutDisplay;
-
-    private Bitmap bitmap;
-
-    private int imgId = 0;
-    private double scale_in = 0.8;//缩小比例
-    private double scale_out = 1.25;//放大比例
-
-    private float scaleWidth = 1;
-    private float scaleHeight = 1;
-    //模式：0：什么都不干；1：拖拽； 2：缩放
-    public static final int NONE = 0;
-    public static final int DRAG = 1;
-    public static final int ZOOM = 2;
-    //声明触发的事件模式
-    private int mode = NONE;
-
-    private Matrix matrix;   //矩阵
-    private Matrix currMatrix; //当前矩阵
-
-    private PointF starPoint;
-    private PointF midPoint;
-
-    private float startDistance;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.drag_layout);
-        //初始化
-        fLayoutDisplay = (FrameLayout) findViewById(R.id.flayout_img_display);
-        lLayoutDisplay = (RelativeLayout) findViewById(R.id.linearLayout_img_display);
-        edtTxtDisplay = (EditText) findViewById(R.id.text);
-        btnZoomin = (Button) findViewById(R.id.btn_min);
-        btnZoomout = (Button) findViewById(R.id.btn_out);
-
-
-        matrix = new Matrix(); //保存拖拽变化
-        currMatrix = new Matrix();// 当前的
-        starPoint = new PointF();//开始点的位置
-
-
-        btnZoomin.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                zoomIn();
-            }
-        });
-        btnZoomout.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                zoomOut();//放大
-            }
-        });
-
-        //给图片绑定监听器哦
-        edtTxtDisplay.setOnTouchListener(new ImageViewOnTouchListener());
-
-    }
-
-    /**放大操作**/
-    private void zoomOut() {
-        reSizeBmp(scale_out);
-        btnZoomin.setEnabled(true);
-    }
-
-    /**缩小操作**/
-    private void zoomIn() {
-        reSizeBmp(scale_in);
-    }
-
-    /**接收传入的缩放比例实现缩放**/
-    private void reSizeBmp(double scale) {
-        //缩放比例
-        scaleWidth = (float) (scaleWidth * scale);
-        scaleHeight = (float) (scaleHeight * scale);
-
-        Matrix matrix = new Matrix();
-        matrix.postScale(scaleWidth, scaleHeight); //设计缩放比例
-
-    }
-
-    /**计算触摸实现缩放**/
-    final class ImageViewOnTouchListener implements OnTouchListener{
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-
-            switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                case MotionEvent.ACTION_DOWN:  //一只手指按下
-                    Log.i(TAG,"一只手指按下");
-                    currMatrix.set(matrix);
-                    starPoint.set(event.getX(), event.getY());
-
-                    mode = DRAG;
-                    break;
-                case MotionEvent.ACTION_POINTER_DOWN: //如果有一只手指按下屏幕，后续又有一个手指按下     // 两只手指按下
-                    Log.i(TAG,"又有一只手指按下");
-
-                    startDistance = distance(event);//记下两点的距离
-                    Log.i(TAG, startDistance+"");
-
-                    if(startDistance > 5f) {  //两个手指之间的最小距离像素大于5，认为是多点触摸
-                        mode = ZOOM;
-                        currMatrix.set(matrix);
-
-                        midPoint = getMidPoint(event); //记下两个点之间的中心点
-                    }
-
-                    break;
-
-                case MotionEvent.ACTION_MOVE:
-
-                    if(mode == DRAG) {  //拖拽模式
-                        Log.i(TAG,"一只手指在拖拽");
-
-                        Log.d("touch point", "x " + event.getX() + "y " + event.getY());
-
-                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)edtTxtDisplay.getLayoutParams();
-                        layoutParams.leftMargin = (int)event.getX();
-                        layoutParams.topMargin = (int)event.getY();
-                        edtTxtDisplay.setLayoutParams(layoutParams);
-                    } else if(mode == ZOOM) {  //缩放模式
-                        Log.i(TAG,"正在缩放");
-
-                        float distance = distance(event);  //两点之间的距离
-                        if(distance > 5f) {
-                            matrix.set(currMatrix);
-                            float cale = distance / startDistance;
-                            matrix.preScale(cale, cale, midPoint.x, midPoint.y);  //进行比例缩放
-                        }
-                    }
-                    break;
-
-                case MotionEvent.ACTION_UP: //最后一只手指离开屏幕后触发此事件
-                case MotionEvent.ACTION_POINTER_UP: //一只手指离开屏幕，但还有一只手指在上面会触此事件
-                    //什么都没做
-                    mode = NONE;
-                    break;
-                default:
-                    break;
-            }
-
-            //两只手指的缩放
-            return true;
-        }
-    }
-
-    /**计算两点之间的距离像素**/
-    private float distance(MotionEvent e) {
-
-        float eX = e.getX(1) - e.getX(0);  //后面的点坐标 - 前面点的坐标
-        float eY = e.getY(1) - e.getY(0);
-        return FloatMath.sqrt(eX * eX + eY * eY);
-    }
-
-    /**计算两点之间的中心点**/
-    private PointF getMidPoint(MotionEvent event) {
-
-        float x = (event.getX(1) - event.getX(0)) / 2;
-        float y = (event.getY(1) - event.getY(0)) / 2;
-        return new PointF(x,y);
-    }
-}
+//package com.example.xiaohu.devtest;
+//import android.app.Activity;
+//import android.content.Intent;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapFactory;
+//import android.graphics.Matrix;
+//import android.graphics.PointF;
+//import android.os.Bundle;
+//import android.util.FloatMath;
+//import android.util.Log;
+//import android.view.MotionEvent;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.view.View.OnTouchListener;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.FrameLayout;
+//import android.widget.ImageView;
+//import android.widget.LinearLayout;
+//import android.widget.RelativeLayout;
+//import android.widget.TextView;
+//
+///**
+// * 缩放图片界面
+// * @author ZHF
+// *
+// */
+//public class MainActivity extends Activity {
+//    public static final String TAG = "ImgDisplayActivity";
+//    //控件声明
+//    private Button btnZoomin, btnZoomout;
+//    private EditText edtTxtDisplay;
+//    private RelativeLayout lLayoutDisplay;
+//    private FrameLayout fLayoutDisplay;
+//
+//    private Bitmap bitmap;
+//
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.drag_layout);
+//        //初始化
+//        fLayoutDisplay = (FrameLayout) findViewById(R.id.flayout_img_display);
+//        lLayoutDisplay = (RelativeLayout) findViewById(R.id.linearLayout_img_display);
+//        edtTxtDisplay = (EditText) findViewById(R.id.text);
+//        btnZoomin = (Button) findViewById(R.id.btn_min);
+//        btnZoomout = (Button) findViewById(R.id.btn_out);
+//
+//
+//
+//    /**计算触摸实现缩放**/
+//    final class ImageViewOnTouchListener implements OnTouchListener {
+//        @Override
+//        public boolean onTouch(View v, MotionEvent event) {
+//
+//            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+//                case MotionEvent.ACTION_DOWN:  //一只手指按下
+//                    Log.i(TAG, "一只手指按下");
+//                    lastPoint.set(event.getRawX(), event.getRawY());
+//                    break;
+//
+//                case MotionEvent.ACTION_MOVE:
+//
+//                    float dx = event.getX() - lastPoint.x;
+//                    float dy = event.getY() - lastPoint.y;
+//
+//                    Log.d("touch point", "x " + event.getX() + "y " + event.getY());
+//
+//                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) edtTxtDisplay.getLayoutParams();
+//                    layoutParams.leftMargin = (int) (event.getX() + dx);
+//                    layoutParams.topMargin = (int) (event.getY() + dy);
+//                    edtTxtDisplay.setLayoutParams(layoutParams);
+//                    lastPoint.x = (int) event.getRawX();
+//                    lastPoint.y = (int) event.getRawY();
+//                    break;
+//                default:
+//                    break;
+//            }
+//            //两只手指的缩放
+//            return true;
+//        }
+//
+//        /**
+//         * 计算两点之间的距离像素*
+//         */
+//        private float distance(MotionEvent e) {
+//
+//            float eX = e.getX(1) - e.getX(0);  //后面的点坐标 - 前面点的坐标
+//            float eY = e.getY(1) - e.getY(0);
+//            return FloatMath.sqrt(eX * eX + eY * eY);
+//        }
+//
+//        /**
+//         * 计算两点之间的中心点*
+//         */
+//        private PointF getMidPoint(MotionEvent event) {
+//
+//            float x = (event.getX(1) - event.getX(0)) / 2;
+//            float y = (event.getY(1) - event.getY(0)) / 2;
+//            return new PointF(x, y);
+//        }
+//    }}}
